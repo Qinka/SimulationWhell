04f283e02204850e0ff66e8051eb5bf86dd7b84c
Qinka <qinka@live.com>
2015-09-22 21:37:25 +0800

    ??

---
+++ include/ulfunc.h
@@ -0,0 +1,19 @@
+/* There are some useless functions, however, which we have to call. */
+
+
+
+
+#ifdef _ULFUNC_H_
+#define _ULFUNC_H_
+
+/* functions a */
+
+void fun57db1();
+void fun5ab1();
+void funa04e();
+
+/* functions */
+
+
+
+#endif
