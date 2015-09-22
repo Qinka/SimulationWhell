ec06e360d7171f0422e3a62ca857ca88350f0472
Qinka <qinka@live.com>
2015-09-22 08:42:41 +0800

    add basic functions just head file

---
+++ include/simulationwhell.h
@@ -0,0 +1,25 @@
+/*  SimulationWhell  */
+
+
+
+
+#ifndef _SIMULATION_H_
+#define _SIMULATION_H_
+
+/* initialize this library */
+unsigned swInit();
+/* stop this library */
+unsigned swStop();
+
+/* basic function */
+/* first function: Hw */
+unsigned funcHw();
+/* second function: doubleItem */
+unsigned funcDouble();
+/* third function, */
+unsigned funcrd();
+
+
+
+
+#endif
